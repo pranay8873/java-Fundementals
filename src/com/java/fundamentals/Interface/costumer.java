59478c2455ca97d097ee5e607f8a7e1a726e4a1e
pranay8873 <pranaypuvaati@gmail.com>
2026-03-27 23:08:36 +0530

    added parameters and methods in coustemer class

--- src/com/java/fundamentals/Interface/costumer.java
+++ src/com/java/fundamentals/Interface/costumer.java
@@ -14,5 +14,14 @@ public class costumer {
     costumer(){
         System.out.println("created coustemer class object");
     }
-    
+    costumer(int id,String Name,byte age,String address,long phoneNumber,String gender,String email,String region){
+        this.id=id;
+        this.Name=Name;
+        this.age=age;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.gender=gender;
+        this.email=email;
+        this.region=region;
+    }
 }
