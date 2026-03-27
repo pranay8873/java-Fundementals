b8d4866ed165209250d09d916efb11ea85188f55
pranay8873 <pranaypuvaati@gmail.com>
2026-03-27 23:03:24 +0530

    implementing interface

---
+++ src/com/java/fundamentals/Interface/costumer.java
@@ -0,0 +1,18 @@
+package com.java.fundamentals.Interface;
+
+import java.util.Scanner;
+
+public class costumer {
+    int id;
+    String Name;
+    byte age;
+    String address;
+    long phoneNumber;
+    String gender;
+    String email;
+    String region;
+    costumer(){
+        System.out.println("created coustemer class object");
+    }
+    
+}
