fa56ddf8806c502f3ae641c29549d1b55384e412
pranay8873 <pranaypuvaati@gmail.com>
2026-03-28 19:29:57 +0530

    implementing interface

--- src/com/java/fundamentals/Interface/Gold_costumer.java
+++ src/com/java/fundamentals/Interface/Gold_costumer.java
@@ -2,6 +2,10 @@ package com.java.fundamentals.Interface;
 
 public class Gold_costumer extends Costumer {
     float Discount=30;
+    Gold_costumer(){
+        System.out.println("you are a gold costumer");
+        System.out.println("since you are a gold costumer you get 30% extra discount");
+    }
     Gold_costumer(int id,String Name,byte age,String address,long phoneNumber,String gender,String email,String region) {
         super();
         System.out.println("you are a gold costumer");
---
+++ src/com/java/fundamentals/Interface/Main.java
@@ -0,0 +1,8 @@
+package com.java.fundamentals.Interface;
+
+public class Main {
+    public static void main(String[] args) {
+        Costumer coustemer1=new Costumer();
+        Gold_costumer costumer2=new Gold_costumer();
+    }
+}
