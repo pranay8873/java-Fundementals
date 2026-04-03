09ffc93ca95cfe70dda73ef46be00551eed02370
pranay8873 <pranaypuvaati@gmail.com>
2026-04-03 20:09:32 +0530

    abstract class

---
+++ src/com/java/fundamentals/Interface/Abstract/Marutisuzuki.java
@@ -0,0 +1,10 @@
+package com.java.fundamentals.Interface.Abstract;
+
+public class Marutisuzuki extends service{
+    String brand;
+    public Marutisuzuki()
+    void display_details(){
+        System.out.println("brand :-"+brand);
+    }
+
+}
---
+++ src/com/java/fundamentals/Interface/Abstract/Service.java
@@ -0,0 +1,20 @@
+package com.java.fundamentals.Interface.Abstract;
+
+
+public abstract class Service{
+    int id;
+    String customerName;
+    String vehicle;
+    String model;
+
+     public Service(int id,String customerName,String vehicle,String model){
+         this.id=id;
+         this.customerName=customerName;
+         this.vehicle=vehicle;
+         this.model=model;
+     }
+    public abstract void details(double amount);
+
+
+
+}
