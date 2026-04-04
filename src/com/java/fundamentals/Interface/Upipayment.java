ce7db4b49d9c1922902d5634c107dd80136b4742
pranay8873 <pranaypuvaati@gmail.com>
2026-04-04 23:13:10 +0530

    upi payment

---
+++ src/com/java/fundamentals/Interface/Upipayment.java
@@ -0,0 +1,8 @@
+package com.java.fundamentals.Interface;
+
+public class Upipayment extends Payment{
+    public UpiPayment(String id, String customerName, String paymentDate, String mode) {
+        super(id, customerName, paymentDate, mode);
+    }
+
+}
